d9a219505b7a4477bf36a4116b9e3d9b7424569c
Janice Escobar <janice.escobar@aol.com>
2026-05-27 21:46:58 -0400

    updated

--- src/main/java/com/pluralsight/ui/SandwichScreen.java
+++ src/main/java/com/pluralsight/ui/SandwichScreen.java
@@ -16,70 +16,30 @@ public class SandwichScreen {
         Sandwich sandwich;
         sandwich = new Sandwich("Sandwich", "Custom", getSandwichSize(), getBread());
         sandwich.setToasted(console.promptForYesNo("Toast bread? [Y] Yes [N] No"));
-        addMeat(sandwich);
-        addCheese(sandwich);
-        addRegularTopping(sandwich);
-        addSauce(sandwich);
-        addSide(sandwich);
+        addTopping(sandwich, ToppingCategory.MEAT);
+        addTopping(sandwich, ToppingCategory.CHEESE);
+        addTopping(sandwich, ToppingCategory.REGULAR_TOPPING);
+        addTopping(sandwich, ToppingCategory.SAUCE);
+        addTopping(sandwich, ToppingCategory.SIDE);
         return sandwich;
     }
-    private void addMeat(Sandwich sandwich){
+    private void addTopping(Sandwich sandwich, ToppingCategory type){
         String name;
-        ArrayList<String> availableMeats = new ArrayList<>(Arrays.asList(ToppingCategory.MEAT.getNames()));
-        do {
-            name = getSelection(availableMeats);
+        ArrayList<String> availableTopping = new ArrayList<>(Arrays.asList(type.getNames()));
+        do{
+            name = getSelection(availableTopping);
             if (name == null) {
                 break;
             }
-            sandwich.addTopping(new Topping(ToppingCategory.MEAT, name));
+            sandwich.addTopping(new Topping(type, name));
             if (console.promptForYesNo("Would you like to add extra " + name + "\n> ")) {
-                sandwich.addExtraTopping(new Topping(ToppingCategory.MEAT, "Extra " + name));
+                sandwich.addExtraTopping(new Topping(type, "Extra " + name));
             }
-            availableMeats.remove(name);
-        }while(!availableMeats.isEmpty());
+            availableTopping.remove(name);
+        }while (!availableTopping.isEmpty());
     }
-    private void addCheese(Sandwich sandwich){
-        String name;
-        ArrayList<String> availableCheese = new ArrayList<>(Arrays.asList(ToppingCategory.CHEESE.getNames()));
-        do {
-            name = getSelection(availableCheese);
-            if (name == null) {
-                break;
-            }
-            sandwich.addTopping(new Topping(ToppingCategory.CHEESE, name));
-            if (console.promptForYesNo("Would you like to add extra " + name + "\n> ")) {
-                sandwich.addExtraTopping(new Topping(ToppingCategory.CHEESE, "Extra " + name));
-            }
-            availableCheese.remove(name);
-        }while(!availableCheese.isEmpty());
-    }
-    private void addRegularTopping(Sandwich sandwich){
-        while(true) {
-            String name = getSelection(ToppingCategory.REGULAR_TOPPING.getNames());
-            if (name == null){
-                break;
-            }
-            sandwich.addTopping(new Topping(ToppingCategory.REGULAR_TOPPING, name));
-        }
-    }
-    private void addSauce(Sandwich sandwich){
-        while(true) {
-            String name = getSelection(ToppingCategory.SAUCE.getNames());
-            if (name == null){
-                break;
-            }
-            sandwich.addTopping(new Topping(ToppingCategory.SAUCE, name));
-        }
-    }
-    private void addSide(Sandwich sandwich){
-        while(true) {
-            String name = getSelection(ToppingCategory.SIDE.getNames());
-            if (name == null) {
-                break;
-            }
-            sandwich.addTopping(new Topping(ToppingCategory.SIDE, name));
-        }
-    }
+    
+
     private BreadType getBread(){
         BreadType[] sizes = BreadType.values();
         for (int i = 0; i < sizes.length; i++){
@@ -101,19 +61,6 @@ public class SandwichScreen {
         }
         return selection.get(choice - 1);
     }
-    private String getSelection(String[] selection){
-        for (int i = 0; i < selection.length; i++){
-            System.out.printf("[%d] %s%n", i + 1, selection[i]);
-        }
-        System.out.printf("[%d] Skip", selection.length);
-        System.out.println("Which one would you like to add?");
-        int choice = console.promptForIntRange("> " , 1, selection.length + 1);
-        if (choice == selection.length){
-            return null;
-        }
-        return selection[choice - 1];
-    }
-
 
 
     private SandwichSize getSandwichSize(){
